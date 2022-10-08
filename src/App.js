69cbaf2cd572d2b8cb9c9cf24195c9295c84a19e
Valeria Balatskaya <valeriabalacka@gmail.com>
2022-10-08 22:50:32 +0200

    link added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import "./Weather.css"
 
 function App() {
   return (
-    <div className="App">
-     <Weather />
-    </div>
+    <><div className="App">
+      <Weather />
+    </div><div>
+        <a href="https://github.com/Valeria-Balatskaya">Coded by Valeria Balatska</a>
+      </div></>
   );
 }
 
